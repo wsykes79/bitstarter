f0b645c502d13adf67ee272bbd985842f96b4a76
wsykes79 <wsykes79@yahoo.com>
2013-07-11 02:35:04 +0000

    switched to string

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var express = require('express'),
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString(fs.readFileSync('index.html')));
+  response.send(String(fs.readFileSync('index.html'));
 });
 
 var port = process.env.PORT || 5000;
